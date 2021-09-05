42d63ef7bc69e8302c6e9dbbd7597d101d79031c
Taisei Toya <tytaisei0102@gmail.com>
2021-09-05 11:21:58 +0200

    feat: db機能を仮作成

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,22 @@
 import express from 'express'
+import mysql from 'mysql'
 
 const app: express.Express = express()
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'client',
+  password: 'password',
+  database: 'timessns',
+})
 
+app.get('/db', (_req, res) => {
+  res.set({ 'Access-Control-Allow-Origin': '*' })
+  connection.query('SELECT * from User', (error, results) => {
+    if (error) throw error
+    res.send(results[0])
+  })
+})
+
+app.listen(5000, () => {
+  console.log('listening on port 5000')
+})
